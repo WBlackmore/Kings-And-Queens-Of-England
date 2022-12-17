22c69661647fc4e6808aaec11a2ead439a127f81
Will Barrett <otterly456@outlook.com>
2022-12-17 22:25:15 +0000

    add date to number function and begin monarchs object

--- app.js
+++ app.js
@@ -1,5 +1,216 @@
-const button = document.querySelector("button");
+/* const button = document.querySelector("button"); */
+export const monarchs = [
+    {
+        name: "William I",
+        start: "December 25, 1066",
+        end: "September 9, 1087",
+        family: "Norman"
+    },
+    {
+        name: "William II", 
+        start: "September 26, 1087",
+        end: "August 2, 1100",
+        family: "Norman"
+    },
+    {
+        name: "Henry I",
+        start: "August 5",
+        end: "December 1, 1135",
+        family: "Norman"
+    },
+    {
+        name: "Stephen I",
+        start: "December 22, 1135",
+        end: "October 25, 1154",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry II",
+        start: "December 19, 1154",
+        end: "July 6, 1189",
+        family: "Plantagenet"
+    },
+    {
+        name: "Richard I",
+        start: "September 3, 1189",
+        end: "April 6, 1199",
+        family: "Plantagenet"
+    },
+    {
+        name: "John I",
+        start: "May 27, 1199",
+        end: "October 19, 1216",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry III",
+        start: "October 28, 1216",
+        end: "November 16, 1272",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward I",
+        start: "November 20, 1272",
+        end: "July 7, 1307",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward II",
+        start: "July 7, 1307",
+        end: "January 25, 1327",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward III", 
+        start: "January 25, 1327",
+        end: "June 21, 1377",
+        family: "Plantagenet"
+    },
+    {
+        name: "Richard II",
+        start: "June 22, 1377",
+        end: "September 29, 1399",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry IV",
+        start: "September 30, 1399",
+        end: "March 20, 1413",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry V",
+        start: "March 21, 1413",
+        end: "August 31, 1422",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry VI",
+        start: "September 1, 1422",
+        end: "March 4, 1461",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward IV",
+        start: "March 4, 1461",
+        end: "October 3, 1470",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry VI",
+        start: "October 3, 1470",
+        end: "April 11, 1471",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward IV",
+        start: "April 11, 1471",
+        end: "April 9, 1483",
+        family: "Plantagenet"
+    },
+    {
+        name: "Edward V",
+        start: "April 9, 1483",
+        end: "June 25, 1983",
+        family: "Plantagenet"
+    },
+    {
+        name: "Richard III",
+        start: "June 26, 1483",
+        end: "August 22, 1485",
+        family: "Plantagenet"
+    },
+    {
+        name: "Henry VII",
+        start: "August 22, 1485",
+        end: "April 21, 1509",
+        family: "Tudor"
+    },
+    {
+        name: "Henry VIII",
+        start: "April 22, 1508",
+        end: "January 28, 1547",
+        family: "Tudor"
+    },
+    {
+        name: "Edward VI",
+        start: "January 28, 1547",
+        end: "July 6, 1553",
+        family: "Tudor"
+    },
+    {
+        name: "Jane I",
+        start: "July 10, 1553",
+        end: "July 19, 1553",
+        family: "Tudor"
+    },
+    {
+        name: "Mary I",
+        start: "July 20, 1553",
+        end: "November 17, 1558",
+        family: "Tudor"
+    },
+    {
+        name: "Elizabeth",
+        start: "November 17, 1558",
+        end: "March 24, 1603",
+        family: "Tudor"
+    },
+    {
+        name: "James I",
+        start: "March 24, 1603",
+        end: "March 27, 1625",
+        family: "Stuart"
+    },
+    {
+        name: "Charles I",
+        start: "March 27, 1625",
+        end: "January 30, 1649",
+        family: "Stuart"
+    },
+    {
+        name: "Charles II",
+        start: "May 29, 1660",
+        end: "February 6, 1685",
+        family: "Stuart"
+    },
+    {
+        name: "James II",
+        start: "February 6, 1685",
+        end: "December 23, 1688",
+        family: "Stuart"
+    },
+]
 
-button.addEventListener("click", ()=> {
-    alert("You CLicked me!")
-})
+
+/*Function to convert any date (day/month/year) into a number. Accepts a 
+  date string in the format "month day, year". Exmaple: "December 25, 1066"*/
+function yearToNumber(date){
+    let newYear = new Date(`"${date}"`)
+    let num = newYear.getTime();
+    return num;
+}
+
+/* Checks a given date againsgt the Monarchs array and pushes to a new array */
+function checkDate(){
+
+}
+
+
+console.log(yearToNumber("December 25, 1066"))
+console.log(monarchs[0])
+
+/*
+{
+    name:
+    start:
+    end:
+    family:
+},
+
+Test dates
+June 13, 1956
+September 3, 789
+December 25, 1066
+March 30, 1688
+*/
---
+++ monarchs.js
@@ -0,0 +1,9 @@
+/* Array to hold objects of each monarch */
+const monarchs = [
+    {
+        name: "William I",
+        start: "December 25, 1066",
+        end: "September 9, 1087",
+        family: "Norman"
+    }
+]
