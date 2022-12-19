65c45dd877162cbf627d4a7e8ac07a43e385936b
Will Barrett <otterly456@outlook.com>
2022-12-19 18:44:30 +0000

    function to check against monarchs array and produce new array

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* const button = document.querySelector("button"); */
 
+
+
 /*Function to convert any date (day/month/year) into a number. Accepts a 
   date string in the format "month day, year". Exmaple: "December 25, 1066"*/
 function yearToNumber(date){
@@ -17,22 +19,40 @@ function numberToDate(num){
 
 /*Returns the day month and year from a given date*/
 function dayMonthYear(fullDate){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", 
+"Septmeber", "October", "November", "December"]
     const date = fullDate;
     const newDate = new Date(date)
     const day = newDate.getDate()
     console.log(day);
     const month = newDate.getMonth()
-    console.log(month);
+    console.log(months[month]);
     const year = newDate.getFullYear();
     console.log(year)
 }
 
-/* Checks a given date againsgt the Monarchs array and pushes to a new array */
-function checkDate(){
+/* Checks a given date against the Monarchs array and pushes to a new array */
+function checkDate(date, obj){
+    const currentMonarchs = [];
 
+    for(let i = 0; i < obj.length; i++){
+
+        let startNum = obj[i].start;
+        let endNum = obj[i].end;
+        let givenDate = yearToNumber(date)
+        let start = yearToNumber(startNum)
+        let end = yearToNumber(endNum)
+        
+        if(givenDate >= start && givenDate <= end){
+            currentMonarchs.push(obj[i]);
+        }
+    }
+    return currentMonarchs;
 }
 
-numberToDate(-28496534325000)
+const test = yearToNumber("August 22, 1485")
+
+console.log(dayMonthYear(test))
 /*
 console.log(yearToNumber("December 25, 1066"))
 console.log(numberToDate(-28496534325000))
